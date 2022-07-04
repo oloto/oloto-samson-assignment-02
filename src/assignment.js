e5be37eb8656e5f2a5a98a965b02d1a88ebb8c94
Samson Oloto <40104447+oloto@users.noreply.github.com>
2022-07-04 12:46:23 +0000

    Attempted questions 1 and 2 (sumOfNumbers and countEvenNumbers)

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+  let sum = arrayOfNumbers.reduce((partialSum, a) => partialSum + a, 0);
+    return sum
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
